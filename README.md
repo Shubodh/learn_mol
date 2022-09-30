8286280c1f16d48ae729614ed909def9c12ae096
Shubodh ADA g08 <p.saishubodh@gmail.com>
2022-09-30 21:41:20 +0530

    gitignore & README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn_mol
 Generating Novel Metal-Chelate Complexes using graph-based Deep Learning
+
+# packages
+python 3.8.13
+pytorch 1.11.0
---
+++ examples/tmQM_dataset_understand.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import jax.numpy as jnp
+import jax
+from jax.example_libraries import optimizers
+import sklearn.manifold, sklearn.cluster
+import rdkit, rdkit.Chem, rdkit.Chem.Draw
+
+soldata = pd.read_csv('/home/saishubodh/2022/pytorch_geometric/data/tmQM/tmQM_y.csv.gz')
+print(soldata.head())
